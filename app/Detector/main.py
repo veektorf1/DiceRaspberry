d205d761b8eee09a1ec6309d581f938a26bb9b92
veektorf1 <wiktor.makowski@student.put.poznan.pl>
2025-01-18 18:55:19 +0000

    Validate threshold change

--- app/Detector/main.py
+++ app/Detector/main.py
@@ -76,7 +76,7 @@ class DiceDetector:
 
 
     self.history.append(result)
-    if self.validate_threshold==(n-1): # n is assigned before appending
+    if self.validate_threshold==(n+1): # n is assigned before appending
       self.confirmed_result = result
   
   def getFinalResult(self,result: dict):
