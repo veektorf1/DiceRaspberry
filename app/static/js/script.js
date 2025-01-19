e6b12e322cb5bc8f34456d74b78ceae3f6ee7a98
Artur Strzelecki <146848435+0Artur1@users.noreply.github.com>
2025-01-20 00:23:19 +0100

    Update script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,8 @@
 var ready = 0;
 var runda = 1;
+var messageElement = null;
+var kropka=0;
+var rzuty=3;
 
 document.querySelectorAll('.clickable-image').forEach(img => {
     img.addEventListener('click', function() {
@@ -10,7 +13,7 @@ document.querySelectorAll('.clickable-image').forEach(img => {
                 .then(response => response.json())
                 .then(data => {
                     if (data.success) {
-                        // Dodatkowe operacje po kliknięciu (jeśli potrzebne)
+                        ;
                     }
                 });
         }
@@ -44,6 +47,10 @@ function loadData() {
             });
         });
 
+    let gracz = runda%2;
+    if(gracz==0)gracz=2;
+    document.getElementById("nrTury").innerText = "Tura: Gracz " + gracz;
+    document.getElementById("nrRzutu").innerText = "Pozostałe rzuty: " + rzuty;
     ready = 1;
 }
 
@@ -77,6 +84,8 @@ function addCellListeners() {
                             document.querySelectorAll('.clickable-image').forEach(function(img) {
                                 img.classList.remove('selected');
                             });
+                            deleteMessage();
+                            rzuty = 3;
                             loadData();
                             ready = 0;
                             runda = runda + 1;
@@ -90,29 +99,59 @@ function addCellListeners() {
     });
 }
 
-function startAutoRefresh(interval) {
-    setInterval(() => {
-        loadData();
-    }, interval);
+function deleteMessage() {
+    if (messageElement) {
+        document.body.removeChild(messageElement);
+        messageElement = null;
+    }
 }
 
-function updateData2(){
-    var button = document.getElementById("update-button");
-    button.disabled = true;
-    fetch('/update-data')
-        .then(response =>response.json())
-        .then(data => {
-            console.log(data);
-            if (data.success) {
-                loadData();
-                button.disabled = false;
-            }
+function createMessage(message){
+    if (!messageElement) {
+        messageElement = document.createElement("div");
+        messageElement.style.position = "fixed";
+        messageElement.style.top = "10%";
+        messageElement.style.left = "50%";
+        messageElement.style.transform = "translate(-50%, -50%)";
+        messageElement.style.backgroundColor = "#f8d7da";
+        messageElement.style.color = "#721c24";
+        messageElement.style.padding = "10px";
+        messageElement.style.border = "2px solid rgb(238, 112, 112)";
+        messageElement.style.borderRadius = "5px";
+        messageElement.style.boxShadow = "0px 4px 6px rgba(0, 0, 0, 0.1)";
+        messageElement.style.zIndex = "1000";
+        messageElement.style.width = "200px";
 
-        })
-        .catch(error =>{
-            console.log(error);
-            button.disabled = false;
-        });
+        document.body.appendChild(messageElement);
+    }
+    messageElement.innerText = message;
+}
+
+function startAutoRefresh(interval) {
+    setInterval(() => {
+        fetch('/check-possible')
+            .then(response => response.json())
+            .then(data => {
+                if(data.wynik=="1") {
+                    deleteMessage();
+                    rzuty = rzuty - 1;
+                    loadData();
+                } else if(data.wynik=="2"){
+                    ready=0;
+                    kropka_text=""
+                    kropka = (kropka+1)%8;
+                    let liczba_kropek=Math.floor(kropka/2);
+                    for(let i=0;i<liczba_kropek;i++)
+                    {
+                        kropka_text = kropka_text + ".";
+                    }
+                    createMessage("Losowanie w toku"+kropka_text);
+                } else if(data.wynik=="3"){
+                    createMessage("Nie masz już ponownych rzutów");
+                }
+
+            })
+    }, interval);
 }
 
 function updateData() {
@@ -120,7 +159,7 @@ function updateData() {
         .then(response => response.json())
         .then(data => {
             if (data.success) {
-                loadData();
+                ;
             }
         });
 }
@@ -128,4 +167,5 @@ function updateData() {
 window.onload = function() {
     loadData();
     ready = 0;
+    startAutoRefresh(200);
 }
